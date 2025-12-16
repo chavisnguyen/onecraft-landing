b9caf20d4ad137fe9621acc4bbcbe94ff8dbda48
Chavis Nguyen <chavisng@g.c>
2025-12-16 22:44:22 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Static export for GitHub Pages deployment
-  output: 'export',
-  // No basePath - deploying to root domain
-  basePath: '',
-  // Disable image optimization for static export
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
